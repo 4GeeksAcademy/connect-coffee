9bca1ffdc2335968c7e02ac45b7916f820299a9c
mouleen <limauropro@gmail.com>
2025-07-23 02:43:56 +0000

    - Nueva estructura de helpers + Helpers para validacion de roles - Adecuacion para Base Gestion de Pagos - Endpoint getProducts ProductDelete - Implementacion de helper para validacion de roles en product

--- src/front/pages/Donations.jsx
+++ src/front/pages/Donations.jsx
@@ -28,30 +28,34 @@ const DonationPage = () => {
     await stripe.redirectToCheckout({ sessionId: session.id });
   };
 
+  const handlePayment = async (e) => {
+    e.preventDefault();
+    let amount=85
+
+    setLoading(true);
+
+    const response = await fetch(backendUrl+'/api/donation/create-checkout-session', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ amount: parseFloat(amount) }),
+    });
+
+    const session = await response.json();
+    const stripe = await stripePromise;
+    await stripe.redirectToCheckout({ sessionId: session.id });
+  };
+
   return (
     <div style={{ maxWidth: '400px', margin: '2rem auto', textAlign: 'center' }}>
-      <h2>💖 Donaciones</h2>
-      <form onSubmit={handleDonate}>
-        <input
-          type="number"
-          placeholder="Un cafecito? (USD)"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-          className='form-control rounded-3 my-2'
-        />
+      <h2>💖 Suscripciones</h2>
+      <form onSubmit={handlePayment}>
+        <div>Tralalaa</div>
         <button type="submit" className='btn btn-warning custom-bg-brown' disabled={loading}>
-          {loading ? 'Redirigiendo...' : 'Donar'}
+          {loading ? 'Redirigiendo...' : 'Pagar'}
         </button>
       </form>
       <div className='text-start'>
-        <br />
-        <h5>Datos de prueba</h5> 
-        <hr />
-        <p><strong>Nro de tarjeta:</strong> 4242 4242 4242 4242</p>
-        <p><strong>Vencimiento:</strong> 12/34</p>
-        <p><strong>CVC:</strong>  Cualquier CVC de tres dígitos (cuatro dígitos si usas una tarjeta American Express)</p>
-        <p><strong>Otros:</strong>  Usa cualquier valor para los demás campos del formulario.</p>
-        <h5 className='text-warning bg-black p-2 rounded-4'>TOMAR NOTA DE ESTOS DATOS PARA EL SIGUIENTE PASO</h5>
+      
       </div>
 
     </div>
